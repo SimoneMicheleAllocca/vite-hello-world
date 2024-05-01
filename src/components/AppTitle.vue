4b773e40142472d543aac7ad144c63857a5a283b
SimoneMicheleAllocca <s.allocca1998@gmail.com>
2024-05-01 11:58:06 +0200

    add components in AppVue

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,13 @@
+<script> 
+export default{
+
+}
+</script>
+<template>
+   
+<link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="./components/index.html">
+</template>
+<style> 
+
+</style>
